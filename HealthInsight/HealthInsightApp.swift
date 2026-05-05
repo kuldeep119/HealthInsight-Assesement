10fcac1ab6d9ae60f03a37fe4fd1a7169168561a
Kuldeep <kuldeepsingh41204@gmail.com>
2026-05-05 23:04:19 +0530

    Initial Commit

---
+++ HealthInsight/HealthInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthInsightApp.swift
+//  HealthInsight
+//
+//  Created by kuldeep Singh on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
